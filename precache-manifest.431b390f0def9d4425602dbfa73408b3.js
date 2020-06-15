d9b48c8f555ef619fdd503c6f7bd197281faab2b
Eishta <eishtamital@gmail.com>
2020-06-15 13:14:54 +0530

    Updates

---
+++ precache-manifest.431b390f0def9d4425602dbfa73408b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6158eaa11a0225a78e13cc0d1b963907",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e2f9091208c7323745ab",
+    "url": "/todo/static/css/main.9411bf28.chunk.css"
+  },
+  {
+    "revision": "3108155e9a40d5695142",
+    "url": "/todo/static/js/2.382208dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.382208dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f9091208c7323745ab",
+    "url": "/todo/static/js/main.e7b95d22.chunk.js"
+  },
+  {
+    "revision": "bda28773891084d0a5dd",
+    "url": "/todo/static/js/runtime-main.5aa3162d.js"
+  }
+]);
